b68306f57abf495f7493e639d0d847c0d5e1883c
Natalya Karpova <knataly0513@gmail.com>
2023-01-03 22:53:00 +0200

    Update ContactItem.jsx

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { Box } from "components/Box";
 
 export const ContactItem = ({ contacts }) => {
     return (<>
         {contacts.map(({ name, id, number }) => (
-            <li key={id}>
+            <Box
+                as="li"
+                display="flex"
+                justifyContent="space-between"
+                mb={2}
+                key={id}>
                 {name}: {number}
-            </li>
+                <Box
+                    as="button"
+                    border="none"
+                    boxShadow="buttonShadow"
+                    borderRadius="normal"
+                    bg="secondary"
+                    color="light"
+                    fontFamily="body"
+                    fontSize={0}
+                    py={1}
+                    px={2}
+                    type="button">
+                    Delete
+                </Box>
+                
+            </Box>
         ))}
     </>)
 };
